8e9045b627d3a4c6c99c965ebdbcf0d65f13b501
lucasdias0812 <99883219+lucasdias0812@users.noreply.github.com>
2022-05-23 10:06:37 -0300

    Add files via upload

---
+++ routes/login.js
@@ -0,0 +1,21 @@
+module.exports = (app)=>{
+    //abrir a view login.ejs
+    app.get('/login', (req,res)=>{
+        res.render('login.ejs')
+    })
+
+    //abrir a view atividades
+    app.post('/login',async(req,res)=>{
+        //conectar com o banco de dados
+        const conexao = require('../config/database')()
+        //importar a model usuarios 
+        const usuarios = require('../models/usuarios')
+        //procurar pelo endereço de email
+        var procurar = await usuarios.findOne({email:req.body.email})
+        if(!procurar){
+          res.send('Email não cadastrado!!')
+        }
+
+        res.render('atividades.ejs')
+    })
+}
